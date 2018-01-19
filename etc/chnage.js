82e641a05a9236942cd88af172ce76e1f2a3acea
takurou <i.taku45@gmail.com>
2018-01-19 15:47:42 +0900

    add etc folder

---
+++ etc/chnage.js
@@ -0,0 +1,3 @@
+const blockElements = document.getElementsByClassName("comparison-block"); //arrow,before-img,after-imgを囲むクラス名
+const elements = document.getElementsByClassName("comparison-arrow"); //境界線のクラス名
+const beforeImages = document.getElementsByClassName("comparison-before-img"); //右側の画像を囲む要素のクラス名
